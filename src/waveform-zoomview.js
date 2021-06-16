6fb17136feba43ff5d3fc23ae74ee40f5f199a7a
Jonathan del Strother <jdelStrother@gmail.com>
2021-06-16 17:55:54 +0100

    Urgh, emit segments events from zoomview while dragging

--- src/waveform-zoomview.js
+++ src/waveform-zoomview.js
@@ -177,14 +177,17 @@ define([
           }
 
           segment.update({ startTime: startTime, endTime: endTime });
+          self._peaks.emit('segments.dragged', segment, startMarker);
         }
       },
 
-      onMouseUp: function(/* mousePosX */) {
+      onMouseUp: function(mousePosX) {
         if (this.dragSegmentId) {
           var segment = this.getDragSegment();
+          var diff = this.mouseDownX - mousePosX;
+          var startMarker = diff > 0;
 
-          self._peaks.emit('segments.drag-segment-complete', segment);
+          self._peaks.emit('segments.dragend', segment, startMarker);
         }
 
         // Set playhead position only on click release, when not dragging.
